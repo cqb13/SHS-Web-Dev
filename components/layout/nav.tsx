ac762fe7855f5a7e72710798d9f64c2a795dd15a
cqb13 <cqb13.main@gmail.com>
2023-08-20 22:18:48 -0400

    User management in admin panel

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -42,7 +42,6 @@ export default function NavBar() {
               });
             } else {
               userIsAdmin(user).then(isAdmin => {
-                console.log(isAdmin);
                 setIsAdmin(isAdmin);
               });
             }
