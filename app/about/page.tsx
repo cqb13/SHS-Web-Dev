b63c49dabb921ca4be0c4af6e39cf0f90e4abcc2
cqb13 <cqb13.main@gmail.com>
2023-08-21 13:53:45 -0400

    Member cards

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,26 +1,68 @@
-"use client"
+"use client";
 
 import MemberCard from "@/components/about/memberCard";
+import { collection, getDocs } from "@firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "@/lib/firebase";
+
+type MemberCardProps = {
+  name: string;
+  icon: string;
+  github: string;
+};
 
 export default function About() {
+  const [members, setMembers] = useState([]);
+  const [memberCardInfo, setMemberCardInfo] = useState<MemberCardProps[]>([]);
+
+  useEffect(() => {
+    getMembers();
+  }, []);
+
+  useEffect(() => {
+    const data: MemberCardProps[] = [];
+    members.forEach((member) => {
+      if (member == "") {
+        return;
+      }
+      data.push({
+        name: member,
+        icon: `https://github.com/${member}.png?size=96`,
+        github: `https://github.com/${member}`
+      });
+    });
+    setMemberCardInfo(data);
+  }, [members]);
 
   const getMembers = () => {
-
-  }
+    const users = collection(db, "users");
+    getDocs(users)
+      .then((querySnapshot) => {
+        const data: any = [];
+        querySnapshot.forEach((doc) => {
+          data.push(doc.data().github);
+        });
+        setMembers(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
   return (
-    <main className="flex flex-col flex-1">
-      <section className="flex flex-col items-center">
-        <h1 className="text-5xl font-medium">Members</h1>
-        <hr className="w-4/5" />
-        <button onClick={getMembers}>get</button>
-        <div className="flex gap-4 m-4">
-          {/*TODO: make this scrape org */}
-          <MemberCard
-            name="cqb13"
-            icon="https://avatars.githubusercontent.com/u/74616162?s=96&v=4"
-            github="https://github.com/cqb13"
-          />
+    <main className='flex flex-col flex-1'>
+      <section className='flex flex-col items-center'>
+        <h1 className='text-5xl font-medium'>Members</h1>
+        <hr className='w-4/5' />
+        <div className='flex gap-4 m-4'>
+          {memberCardInfo.map((member) => (
+            <MemberCard
+              key={member.name}
+              name={member.name}
+              icon={member.icon}
+              github={member.github}
+            />
+          ))}
         </div>
       </section>
     </main>
