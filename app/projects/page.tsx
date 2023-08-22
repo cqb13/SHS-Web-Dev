c2daf5ad2eb2977727ba31c3ca410a5ea1d2500c
cqb13 <cqb13.main@gmail.com>
2023-08-22 11:30:09 -0400

    Projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,8 +1,43 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import ProjectCard from "@/components/projects/projectCard";
+
 export default function Projects() {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>Scrapes our github / github of members, for projects with specific json file and adds them and their details here (crystal addon stuff)</h1>
-      </main>
-    )
-  }
-  
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/Website-Club/repos")
+      .then((res) => res.json())
+      .then((data) => {
+        const filtered = data.filter((repo: any) => {
+          return !repo.fork && !repo.archived && !repo.disabled;
+        });
+        setProjects(filtered);
+      });
+  }, []);
+
+  return (
+    <main className='flex flex-col flex-1 p-4 items-center'>
+      <section className="w-4/5">
+        {projects.length > 0 ? (
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+            {projects.map((project: any) => (
+              <ProjectCard
+                key={project.id}
+                name={project.name}
+                description={project.description}
+                url={project.html_url}
+                homepage={project.homepage}
+                stars={project.stargazers_count}
+                topics={project.topics}
+              />
+            ))}
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </section>
+    </main>
+  );
+}
